4737dff0709464b496ad2721f4ccf2da3f1f5152
Joao lucas <joaolucaspereira2002@gmail.com>
2026-05-22 15:28:57 -0300

    Criando nova branch de dev

--- render-deploy.sh
+++ render-deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 set -e # Encerra o script ao primeiro erro 
 # Migrações com Flask-Migrate 
+flask --app src.app db stamp head || true
 flask --app src.app db upgrade 
 # Inicia com Gunicorn 
 gunicorn src.wsgi:app
+
