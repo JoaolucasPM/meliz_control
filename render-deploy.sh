36dfdffe37f91fa3c3dfdd9379e631da70f6e9fc
Joao lucas <joaolucaspereira2002@gmail.com>
2026-05-26 11:38:25 -0300

    corrigindo o sh

--- render-deploy.sh
+++ render-deploy.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "Sincronizando Alembic..."
-flask --app src.app db stamp head || true
-
-echo "Aplicando migrations..."
 flask --app src.app db upgrade
 
-echo "Iniciando aplicação..."
-gunicorn --workers 1 --threads 2 src.wsgi:app
+gunicorn --workers 1 src.wsgi:app
