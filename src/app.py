77103ee849410afc4b686dd2b22476ebd8866677
Joao lucas <joaolucaspereira2002@gmail.com>
2026-05-26 11:24:07 -0300

    limpando a cache

--- src/app.py
+++ src/app.py
@@ -15,7 +15,7 @@ def iniciar_bot():
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+#    app = Flask(__name__, instance_relative_config=True)
 #    app.config.from_mapping(
 #        SECRET_KEY='dev',
 #        SQLALCHEMY_DATABASE_URI = "sqlite:///project.db",
